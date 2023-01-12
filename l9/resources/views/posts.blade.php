8a9372ca9e235663ed6317d534f17379bd46b74a
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2023-01-12 21:23:11 +0100

    Video 34 Starting JS filters, dinamic posts OK

--- l9/resources/views/posts.blade.php
+++ l9/resources/views/posts.blade.php
@@ -1,20 +1,14 @@
 <x-layout>
-@foreach($posts as $post)
+@include('_posts-header')
+            
 
-<article>
-    <h1>
-        <a href="/posts/{{ $post->slug }}">
-            {!! $post->title !!}
-        </a>
-    </h1>
-        <p>
-        By  <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> 
-        in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            @if($posts->count())
+               <x-post-grid :posts="$posts"/>
+            @else
+                <p class="text-center">No posts yet. please check back later.</p>
+            @endif
+            
 
-    <div>
-        {!! $post->excerpt !!}
-    </div>
-</article>
-@endforeach
+        </main>
 </x-layout>
