8f143236268f290fd7fc63a70131ce2c1ce7f011
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2023-01-15 01:36:35 +0100

    video 56, form comment designed

--- l9/resources/views/components/post-comment.blade.php
+++ l9/resources/views/components/post-comment.blade.php
@@ -1,17 +1,19 @@
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+@props(['comment'])
+<x-panel class="bg-gray-100">
+<article class="flex space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
     <div>
     <header class="mb-4">
-        <h3 class="font-bold">John Doe</h3>
+        <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
     </header>
     <p>
-        Lorem ipsum dolor sit amet consectetur aqui adipisci omni et voluptae Lorem ipsum dolor sit amet consectetur
-        aqui adipisci omni et voluptae Lorem ipsum dolor sit amet consectetur aqui adipisci omni et voluptae
+        {{ $comment->body }}
     </p>
     </div>
 </article>
+</x-panel>
